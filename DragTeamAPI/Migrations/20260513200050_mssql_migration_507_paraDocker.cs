0fed4411314715705e16eb3b04ea8a6bfbcbea30
Bru-Fasani <brunafasani.arts@gmail.com>
2026-05-13 17:11:45 -0300

    Suporte Docker Compose e ajustes para SQL Server em container
    Adicionados arquivos de configuração do Docker Compose, perfil de execução para containers e migration para compatibilidade com SQL Server em ambiente Docker. Ajustada string de conexão no appsettings.json e referência ao projeto docker-compose no csproj.

---
+++ DragTeamAPI/Migrations/20260513200050_mssql_migration_507_paraDocker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DragTeamAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssql_migration_507_paraDocker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
